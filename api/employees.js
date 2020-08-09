7931c0b55675913de6d2ce9400114501576356f6
christopher-1989 <64772696+christopher-1989@users.noreply.github.com>
2020-08-09 15:40:02 +1000

    added errorhandler and body-parser

--- api/employees.js
+++ api/employees.js
@@ -4,5 +4,15 @@ const employeesRouter = express.Router()
 
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
 
+employeesRouter.get('/', (req, res, next) => {
+    db.all('SELECT * FROM Employee WHERE is_current_employee = 1',
+    (err, employees) => {
+        if (err) {
+            next(err)
+        } else {
+            res.status(200).json({employees})
+        }
+    })
+})
 
 module.exports = employeesRouter
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const apiRouter = require('./api/api')
 const morgan = require('morgan')
+const errorHandler = require('errorhandler')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 const PORT = process.env.PORT || 4000
+app.use(bodyParser.json())
 app.use(morgan('dev'))
-
 app.use('/api', apiRouter)
+app.use(errorHandler())
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
